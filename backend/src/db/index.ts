8d61ffa33bf26641b8a342f3e20967090ad5da16
GuillemotClement <guillemot.clement@protonmail.com>
2026-05-31 16:57:54 +0200

    fix: setting DB SSL

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,10 +4,10 @@ import * as schema from './schema';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false } // Required for Supabase
-      : false, // Disabled for local Docker
+  ssl: false, // connexion interne via le docker => pas besoin de SSL
+  // process.env.NODE_ENV === 'production'
+  //   ? { rejectUnauthorized: false } // Required for Supabase
+  //   : false, // Disabled for local Docker
 });
 
 export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
